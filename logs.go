7126981b3f8ac5b6cbdb5288438accd10f03a7dd
Vishal <vishal@Dilips-MacBook.local>
2025-02-03 14:11:53 +0530

    init commit

---
+++ logs.go
@@ -0,0 +1,13 @@
+// mathutils/mathutils.go
+package accesslogs
+
+// Add adds two integers
+func Add(a, b int) int {
+    return a + b
+}
+
+// Subtract subtracts the second integer from the first
+func Subtract(a, b int) int {
+    return a - b
+}
+
